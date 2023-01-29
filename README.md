f3cb8987f51fddb33047127583973fa99526b4c6
Lord-Chun-Ma <muthuselvan@outlook.com>
2023-01-29 20:33:55 +0530

    v2 of the app, data persistence using API

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# About Books app
+# About Books v2 app
 
-> This app makes use of props, event and state systems to add a book from user input and be displayed in a list
+> In version 2 of our books app, we are making the data persist even through reload and relaunching the app with the help of an API server
 
-> User then will be able to remove the said book or click on the ✏ button to edit and update the title of the book
+> so initially we set up an API server using npm packages json-server which can be used for development purposes
 
-> However, the data in this app is local and non persistent as when user reloads the app, the data is reset everytime
+> additionally, we added a script to the package.json file to watch the port 3001
 
-> In the upcoming successive iterations of the app I will learn and implement presistent data using outside API server
+> added a local file db.json where all the persistent data in our app is stored
+
+> An additional file of api.http has been created for all the requests we will be using in this app
+
+> I have also added an extension called REST client to view the responses of our requests directly in VSCode client to evaluate further
+
+> updated the code in App.js so that we make use of API calls using axios to our API server to fetch, edit, delete data from our local Json file
+
+> In this project, i have learnt about the useEffect function and implemented it in the initial GET request so that we are not stuck in an infinite loop in the browser(check the code for comments)
--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const editBookByID = (id, newTitle) => {
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+  //DONT DO THIS:
+  // DO NOT CALL the fetchBooks function directly like this:
+  // fetchBooks();
+  // This will cause the app component to rerender and call the function again and repeat the cycle
+  // we end up in an infinite loop
+
+  const editBookByID = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
     setBooks(updatedBooks);
   };
 
-  const deleteBookByID = (id) => {
+  const deleteBookByID = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 999), title },
-    ]; //Since the key and values of the object is title, we can just use title instead of title:title
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title: title,
+      // you can also use just title since key and value are the same
+    });
+
+    const updatedBooks = [...books, response.data]; //Since the key and values of the object is title, we can just use title instead of title:title
     setBooks(updatedBooks);
   };
 
